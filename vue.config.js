e89f5395067342bc9cd0c1bdee6d7ae7e1d1c320
toannd <sitdownrightnow.dev@gmail.com>
2021-07-29 07:27:14 +0700

    chore: setup dev server proxy

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,19 @@
 module.exports = {
+  devServer: {
+    port: 9000,
+    proxy: {
+      '^/api': {
+        target: 'http://localhost:5000',
+        ws: true,
+        changeOrigin: true,
+      },
+      '^/files': {
+        target: 'http://localhost:5000',
+        ws: true,
+        changeOrigin: true,
+      },
+    },
+  },
   transpileDependencies: [
     'vuetify',
   ],
